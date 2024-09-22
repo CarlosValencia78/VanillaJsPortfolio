df81231e1c23a93b5c71e55113a4dabb5be3b629
carlosgvalencia78@gmail.com <carlosgvalencia78@gmail.com>
2024-09-22 17:34:48 -0400

    hook up express and restructure folders

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = express();
-
-//MIDDLEWARE
-app.use(express.static(`${__dirname}/public`));
-
 const port = process.env.PORT || 3000;
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
+
+//MIDDLEWARE
+app.use(express.static(`${__dirname}/public`));
 
 mongoose
   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,38 +27,6 @@ app.use(
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-});
-
-// app.get("/styles.scss", (req, res) => {
-//   res.sendFile(__dirname + "/styles.scss");
-// });
-
-app.get("/script.js", (req, res) => {
-  res.sendFile(__dirname + "/script.js");
-});
-
-// app.get("/_colors.scss", (req, res) => {
-//   res.sendFile(__dirname + "/_colors.scss");
-// });
-
-// app.get("/_stars.scss", (req, res) => {
-//   res.sendFile(__dirname + "/_stars.scss");
-// });
-
-app.get("/assets/images/CVAL3137.JPG", (req, res) => {
-  res.sendFile(__dirname + "/assets/images/CVAL3137.JPG");
-});
-
-app.get("/assets/images/hubble-spiral.jpg", (req, res) => {
-  res.sendFile(__dirname + "/assets/images/hubble-spiral.jpg");
-});
-
-app.get("/assets/images/github-icon.png", (req, res) => {
-  res.sendFile(__dirname + "/assets/images/github-icon.png");
-});
-
-app.get("/assets/images/linkedin-icon.png", (req, res) => {
-  res.sendFile(__dirname + "/assets/images/linkedin-icon.png");
 });
 
 app.listen(port, () => {
